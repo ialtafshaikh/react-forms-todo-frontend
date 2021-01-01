fb48ab696d78ad5506571c7fd1556a229a729d85
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-01 16:29:09 +0530

    endpoint chnaged to hosted todo backend

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,5 +1,5 @@
-// const BASEURL = "https://todo-rest-api-backend.herokuapp.com/";
-const BASEURL = "http://localhost:3001/";
+const BASEURL = "https://todo-rest-api-backend.herokuapp.com/";
+// const BASEURL = "http://localhost:3001/";
 export const endpoint = BASEURL + "todos/";
 export const signup = BASEURL + "signup";
 export const login = BASEURL + "login/username";
